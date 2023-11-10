2d5dfecb16f945d426d28e5d4981134e856063ab
Henrique Rodrigues <71653239+HenriqueCCR@users.noreply.github.com>
2023-11-10 14:45:25 +0000

    Added ovveride messages for phone functions

--- app/src/main/java/rodrigues/henrique/myapplication2/MainActivity.java
+++ app/src/main/java/rodrigues/henrique/myapplication2/MainActivity.java
@@ -3,6 +3,7 @@ package rodrigues.henrique.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Activity Lifecycle","onCreate");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity Lifecycle","onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity Lifecycle","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity Lifecycle","onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity Lifecycle","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity Lifecycle","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity Lifecycle","onRestart");
     }
 }
