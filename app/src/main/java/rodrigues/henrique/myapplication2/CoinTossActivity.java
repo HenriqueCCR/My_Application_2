037adbb78a1d6dc39198dd3778199096b24ec193
Henrique Rodrigues <71653239+HenriqueCCR@users.noreply.github.com>
2023-11-10 16:19:00 +0000

    creating button to flip a coin but Default activity can not be found

---
+++ app/src/main/java/rodrigues/henrique/myapplication2/CoinTossActivity.java
@@ -0,0 +1,66 @@
+package rodrigues.henrique.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import java.util.Random;
+import android.widget.TextView;
+
+public class CoinTossActivity extends AppCompatActivity {
+    public Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_toss);
+        Log.i("Activity Lifecycle","onCreate");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity Lifecycle","onPause");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity Lifecycle","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity Lifecycle","onResume");
+        TextView coinTossView = findViewById(R.id.coinTossView);
+
+        String result = getCoinToss();
+
+        coinTossView.setText(result);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity Lifecycle","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity Lifecycle","onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity Lifecycle","onRestart");
+    }
+    private String getCoinToss(){
+        if(random.nextBoolean()){
+            return getString(R.string.coinTossResult1);
+        }
+        return getString(R.string.coinTossResult2);
+    }
+}
--- app/src/main/java/rodrigues/henrique/myapplication2/MainActivity.java
+++ app/src/main/java/rodrigues/henrique/myapplication2/MainActivity.java
@@ -2,65 +2,21 @@ package rodrigues.henrique.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import java.util.Random;
-import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    public Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Activity Lifecycle","onCreate");
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("Activity Lifecycle","onPause");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("Activity Lifecycle","onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("Activity Lifecycle","onResume");
-        TextView coinTossView = findViewById(R.id.coinTossView);
-
-        String result = getCoinToss();
-
-        coinTossView.setText(result);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("Activity Lifecycle","onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("Activity Lifecycle","onDestroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("Activity Lifecycle","onRestart");
-    }
-    private String getCoinToss(){
-        if(random.nextBoolean()){
-            return getString(R.string.coinTossResult1);
-        }
-        return getString(R.string.coinTossResult2);
+    /**Called when user touches the button*/
+    public void openCoinToss(View view){
+        //Do something in response to button click
+        Intent openCoinTossIntent = new Intent(getApplicationContext(), CoinTossActivity.class);
+        startActivity(openCoinTossIntent);
     }
 }
