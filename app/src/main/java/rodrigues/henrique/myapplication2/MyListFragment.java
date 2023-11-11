574a4292d8f37212375df4b4fdd214d937589c0c
Henrique Rodrigues <71653239+HenriqueCCR@users.noreply.github.com>
2023-11-11 17:34:59 +0000

    Can now rotate device to get a landscape layout :)

--- app/src/main/java/rodrigues/henrique/myapplication2/MyListFragment.java
+++ app/src/main/java/rodrigues/henrique/myapplication2/MyListFragment.java
@@ -2,6 +2,7 @@ package rodrigues.henrique.myapplication2;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.fragment.app.ListFragment;
 
 import android.content.Intent;
@@ -11,6 +12,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MyListFragment extends ListFragment {
+    int mCurCheckPosition = 0;
+    boolean mSingleActivity;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -20,6 +24,21 @@ public class MyListFragment extends ListFragment {
                 DummyData.DATA_HEADINGS));
 
         getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        View contentFrame = getActivity().findViewById(R.id.content);
+        mSingleActivity = contentFrame != null && contentFrame.getVisibility() == View.VISIBLE; //setting boolean value depending if landscape or note
+
+        if (savedInstanceState != null) {
+            //restore last state for checked position
+            mCurCheckPosition = savedInstanceState.getInt("curChoice",0);
+        }
+
+        if (mSingleActivity) {
+            showContent(mCurCheckPosition);
+        }
+        else {
+            getListView().setItemChecked(mCurCheckPosition,true);
+        }
     }
 
     @Override
@@ -27,16 +46,45 @@ public class MyListFragment extends ListFragment {
         showContent(position); //this tells where in the list we clicked
     }
     void showContent(int index) {
-        //Create an intent for starting the DetailsActivity
-        Intent intent = new Intent();
+        mCurCheckPosition = index;
 
-        //explicitly set the activity context and class
-        //associated with the intent (context, class)
-        intent.setClass(getActivity(), ItemActivity.class);
+        if(mSingleActivity) {
+            getListView().setItemChecked(index,true);
 
-        //pass the current position
-        intent.putExtra("index", index);
+            //check what fragment is currently shown, replace if needed.
+            ListItemFragment content = (ListItemFragment) getFragmentManager().findFragmentById(R.id.content);
+            if (content == null || content.getShownIndex() != index){
+                //Make new fragment show this selection
 
-        startActivity(intent);
+                content = ListItemFragment.newInstance(index);
+
+                //Execute a transaction, replacing any existing fragment
+                //with this one inside the frame
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.content, content);
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                fragmentTransaction.commit();
+            }
+        }
+        else {
+            //Create an intent for starting the DetailsActivity
+            Intent intent = new Intent();
+
+            //explicitly set the activity context and class
+            //associated with the intent (context, class)
+            intent.setClass(getActivity(), ItemActivity.class);
+
+            //pass the current position
+            intent.putExtra("index", index);
+
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("curChoice", mCurCheckPosition);
     }
 }
