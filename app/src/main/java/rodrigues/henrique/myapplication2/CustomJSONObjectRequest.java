2d7bfd36174031a44b42309456a249dd6536c1e3
Henrique Rodrigues <71653239+HenriqueCCR@users.noreply.github.com>
2024-02-12 19:46:13 +0000

    Added CustomItemImageRequest.java

---
+++ app/src/main/java/rodrigues/henrique/myapplication2/CustomJSONObjectRequest.java
@@ -0,0 +1,33 @@
+package rodrigues.henrique.myapplication2;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+public class CustomJSONObjectRequest implements Response.Listener<JSONObject>, Response.ErrorListener {
+    private VolleyJSONObjectResponse mVolleyJSONObjectResponse;
+    private String mTag;
+    private JsonObjectRequest mJasonObjectRequest;
+
+    public JsonObjectRequest getJsonObjectRequest(){
+        return mJasonObjectRequest;
+    }
+
+    public CustomJSONObjectRequest(int pMethod, String pUrl, JSONObject pJsonObject, String pTag, VolleyJSONObjectResponse pVolleyObjectResponse){
+        this.mVolleyJSONObjectResponse = pVolleyObjectResponse;
+        this.mTag = pTag;
+        mJasonObjectRequest = new JsonObjectRequest(pMethod, pUrl, pJsonObject, this, this);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError pError) {
+        mVolleyJSONObjectResponse.onError(pError, mTag);
+    }
+
+    @Override
+    public void onResponse(JSONObject pResponse) {
+        mVolleyJSONObjectResponse.onResponse(pResponse, mTag);
+    }
+}
