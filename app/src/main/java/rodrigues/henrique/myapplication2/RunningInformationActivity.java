fcb3b7a56ada8815ff35f738752a07ea97351356
Henrique Rodrigues <71653239+HenriqueCCR@users.noreply.github.com>
2024-05-07 13:31:28 +0100

    Added return button from tool bar for Android in Information Home Page.
    Changed SelectTime to hint text from user testing suggestions

--- app/src/main/java/rodrigues/henrique/myapplication2/RunningInformationActivity.java
+++ app/src/main/java/rodrigues/henrique/myapplication2/RunningInformationActivity.java
@@ -2,12 +2,16 @@ package rodrigues.henrique.myapplication2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class RunningInformationActivity extends AppCompatActivity {
     private Button longDistanceButton, mediumDistanceButton, shortDistanceButton, hillProgressionButton, tempoButton;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,21 @@ public class RunningInformationActivity extends AppCompatActivity {
         shortDistanceButton = findViewById(R.id.shortDistanceInfoButton);
         hillProgressionButton = findViewById(R.id.hillProgressionInfoButton);
         tempoButton = findViewById(R.id.tempoInfoButton);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void openLongDistanceInfo(){
 
     }
